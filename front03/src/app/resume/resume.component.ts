87fd517910101a4f05a89cc39189bf5d967d0187
MosNanthaphop <nanthaphop.weerahong@gmail.com>
2025-02-27 02:45:09 +0700

    Finished Phase1

--- front03/src/app/resume/resume.component.ts
+++ front03/src/app/resume/resume.component.ts
@@ -9,51 +9,5 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./resume.component.css']
 })
 export class ResumeComponent {
-  name = 'Nanthaphop Weerahong';
-  title = 'Computer Science Student';
-  phone = '081-116-5125';
-  email = 'nanthaphop.weerahong@gmail.com';
-  address = 'Nongbua, Banphaeo, Samut Sakhon 74120';
-  
-  leftSections = [
-    {
-      title: 'Contact',
-      items: [
-        'Phone: 081-116-5125',
-        'Email: nanthaphop.weerahong@gmail.com',
-        'Address: Nongbua, Banphaeo, Samut Sakhon 74120'
-      ]
-    },
-    {
-      title: 'Education',
-      items: [
-        'Silpakorn University - BSc Computer Science (2022-2026, GPA 3.17)',
-        'Banphaeowittaya School - Sciences and Mathematics (2019-2022, GPAX 3.62)'
-      ]
-    },
-    {
-      title: 'Programming Skills',
-      items: [
-        'Java, C, Python, Dart, Go, PHP, SQL, HTML, CSS, JavaScript'
-      ]
-    },
-    {
-      title: 'Languages',
-      items: [
-        'Thai (Native)',
-        'English (Intermediate - B1)'
-      ]
-    }
-  ];
-  
-  rightSections = [
-    {
-      title: 'Experience',
-      items: [
-        'E-commerce Web Application - Full Stack Developer (HTML, CSS, JavaScript)',
-        'Download File with Zero Copy and Multithreading - Java Developer',
-        'Currency Converter App - Mobile Developer (Dart, JavaScript)'
-      ]
-    }
-  ];
+  profileImage = 'assets/img/person.png';
 }
